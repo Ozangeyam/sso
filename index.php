0030be0f071bf9e4f1088779602270336c193d99
Snoodie <45816295+Ozangeyam@users.noreply.github.com>
2022-06-06 06:04:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -654,6 +654,18 @@ s0.parentNode.insertBefore(s1,s0);
 <li><a class="py-1 d-block" href="testimonials.html">Testimonials</a></li>
 </ul>
 </div>
+<div class="col-lg-5">
+<div id="google_translate_element"></div>
+
+
+  <script type="text/javascript">
+        function googleTranslateElementInit() {
+          new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+        }
+  </script>
+
+                            <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+                        </div>
 <div class="col-md-4 mb-md-0 mb-4">
 <h2 class="footer-heading">Need Help?</h2>
 <ul class="list-unstyled">

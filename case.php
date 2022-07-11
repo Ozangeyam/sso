b452b0cc6cd716861ac665e67273e3d2a780c4fb
Snoodie <45816295+Ozangeyam@users.noreply.github.com>
2022-07-11 19:36:40 +0800

    Update case.php

--- case.php
+++ case.php
@@ -90,7 +90,7 @@
         (function(){
         var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
         s1.async=true;
-        s1.src='https://embed.tawk.to/624c76ef2abe5b455fc51248/default';
+        s1.src='https://embed.tawk.to/62cc059d7b967b117998fb70/1g7mfbshg';
         s1.charset='UTF-8';
         s1.setAttribute('crossorigin','*');
         s0.parentNode.insertBefore(s1,s0);
